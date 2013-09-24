386fff1350e88c161d27df67983a7fb468653f84
Ibruce1 <Ibruce1@mail.csuchico.edu>
2013-09-23 21:27:56 -0700

    Update formMaster.py
    Converts time units to seconds.
    Does save to file.

--- formMaster.py
+++ formMaster.py
@@ -2,6 +2,19 @@ def form():#called when the form.html is
     return dict()#just prints form.html
     
 def form2():#calls form2.html
+    timeout = request.vars.timeout
+    timeUnit = request.vars.timeUnit
+    int(timeout)
+    if timeUnit == 'minutes':
+        timeout=timeout*60
+    elif timeUnit =='hours':
+        timeout=timeout*3600
+    elif timeUnit == 'days':
+        timeout=timeout*86400 
     configFile = open('config.txt','w') #should open a file to write to
-    configFile.write(request.vars.bob)  #should write the value from the form to the file
+    configFile.write(timeout)  #should write the value from the form to the file
+    configFile.write('\n')
+    configFile.write(request.vars.ssidName)
+    configFile.write('\n')
     return dict()#displays form2.html
+
