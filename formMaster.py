a7981fb0ed2808e84e032dc5d772fef79d5931e8
Ibruce1 <Ibruce1@mail.csuchico.edu>
2013-09-24 20:44:06 -0700

    Update formMaster.py
    Fixed bug, multiplying the timeout variable only wrote the numeric characters multiple times.

--- formMaster.py
+++ formMaster.py
@@ -4,13 +4,14 @@ def form():#called when the form.html is
 def form2():#calls form2.html
     timeout = request.vars.timeout
     timeUnit = request.vars.timeUnit
-    int(timeout)
+    timeout=int(timeout)
     if timeUnit == 'minutes':
         timeout=timeout*60
     elif timeUnit =='hours':
         timeout=timeout*3600
     elif timeUnit == 'days':
         timeout=timeout*86400 
+    timeout=str(timeout)
     configFile = open('config.txt','w') #should open a file to write to
     configFile.write(timeout)  #should write the value from the form to the file
     configFile.write('\n')
